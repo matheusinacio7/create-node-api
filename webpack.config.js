fed9bd06fd9cc72d1a7c52d33db4e63838dbe82e
heyset <c.inaciomatheus@gmail.com>
2021-10-25 16:28:55 -0300

    Adds banner

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   entry: './src/index.ts',
   module: {
@@ -16,4 +18,7 @@ module.exports = {
     filename: 'dist.js',
     path: __dirname,
   },
+  plugins: [
+    new webpack.BannerPlugin({ banner: '#! /usr/bin/env node', raw: true }),
+  ],
 };
