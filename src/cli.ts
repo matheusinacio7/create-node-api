0b4ffc4f1be2d8aadee6ea649a63cd9b0881f9c8
heyset <c.inaciomatheus@gmail.com>
2021-10-25 23:37:11 -0300

    Upgrades version

--- src/cli.ts
+++ src/cli.ts
@@ -28,7 +28,7 @@ export default async function entry() {
 
     const program = new Command();
 
-    program.version('0.0.16');
+    program.version('0.0.17');
   
     program.argument('<folder>');
 
